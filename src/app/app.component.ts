2b26914beeb5aeda96de5a873609b1d4c7ec0f59
aditya2903 <adisharma2903@gmail.com>
2022-03-28 00:32:33 -0400

    update pkg json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { asapScheduler, delay, Observable, of } from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'testapp';
+
+  runAsync() {
+    setTimeout(() => {
+      console.log('settimeout');// macro task
+    }, 0);
+    this.returnObservable().subscribe(
+      console.log// synchronous task of or from operator might be synchronous
+    );
+    Promise.resolve("promise value").then(console.log);// micro task
+    // macro task has lower priority than micro task
+  }
+  returnObservable(): Observable<string> {
+    return of('stream value').pipe(
+      delay(0, asapScheduler)
+    );
+  }
 }
